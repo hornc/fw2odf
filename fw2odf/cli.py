0e8ae5d40f421a73fbcbf4de9e1cf32698ef9b7f
Chales Horn <charles.horn@gmail.com>
2023-02-17 21:57:36 +1300

    use argparse for args

--- fw2odf/cli.py
+++ fw2odf/cli.py
@@ -1,5 +1,4 @@
 import argparse
-import sys
 
 from pathlib import Path
 
@@ -11,21 +10,26 @@ from fw2odf.finalwriter import FW, Rule
 from fw2odf.symbol import from_symbol
 
 
-"""
+DESC = """
 Amiga Final Writer to ODF conversion
 
 """
 
 
 def main():
-    fname = sys.argv[1]
-    outfile = Path(fname).with_suffix('.odt') if fname.endswith('.fw') else Path(fname + '.odt')
+    parser = argparse.ArgumentParser(description=DESC)
+    parser.add_argument('source', help='Input FinalWriter file (.fw)', type=argparse.FileType('rb'))
+    parser.add_argument('-d', '--debug', help='Debug', action='store_true')
+    args = parser.parse_args()
 
-    with open(fname, 'rb') as f:
-        fwdoc = FW(f) 
+    f = args.source
+    outfile = Path(f.name).with_suffix('.odt') if f.name.endswith('.fw') else Path(f.name + '.odt')
 
-    print('DEBUG:', fwdoc.raw)
-    print('DEBUG:', fwdoc.raw.getsize())
+    fwdoc = FW(f)
+
+    if args.debug:
+        print('DEBUG:', fwdoc.raw)
+        print('DEBUG:', fwdoc.raw.getsize())
 
     textdoc = OpenDocumentText()
     # Justified style
@@ -47,7 +51,8 @@ def main():
         current_style = fwdoc.style_from_attr(t.attr)
         textdoc.automaticstyles.addElement(current_style)
         if 'symbol' in str(current_style.getAttribute('name').lower()):
-            print(f'SYMBOL got "{t.text}" --> "{from_symbol(t.text)}"')
+            if args.debug:
+                print(f'SYMBOL got "{t.text}" --> "{from_symbol(t.text)}"')
             text = Span(stylename=current_style, text=from_symbol(t.text))
         elif t.text == '\t':
             text = Tab()
